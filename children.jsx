5e325b06c4c4f51d05d5fe40b7709af0402624fc
Ana Gauna <amgauna@gmail.com>
2021-11-03 09:54:03 -0300

    Create children.jsx

---
+++ children.jsx
@@ -0,0 +1,9 @@
+React.createElement("div", { className: "red" }, "Children Text");
+React.createElement(MyCounter, { count: 3 + 5 });
+
+<div className="red">Children Text</div>;
+<MyCounter count={3 + 5} />;
+var GameScores = {player1: 2,player2: 5};
+<DashboardUnit data-index="2">
+<h1>Scores</h1><Scoreboard className="results" scores={GameScores} />
+</DashboardUnit>;
